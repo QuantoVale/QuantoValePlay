dcbaf5f371fb702203474667c17d8aa609b20208
ruanherculano <ruannawe95@gmail.com>
2015-09-19 23:56:55 -0300

    add function parse controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+require 'httparty'
+
+class ContratosController < AplicationController
+
+    def ParseContrato
+        response = HTTParty.get('http://dados.gov.br/dataset/compras-publicas-do-governo-federal')
+        response.each do |item|
+            u = Contrato.new(
+                :identificadorContrato => item["Identificador do Contrato"],
+                :uasg => item["Uasg"],
+                :modalidadeLicitacao => item["Modalidade da Licitação"],
+                :codigoContrato => item["Modalidade da Licitação"],
+                :licitacaoAssociada => item["Licitação Associada"],
+                :ModalidadeLicitacao => item["Modalidade da Licitação"],
+                :objeto => item["Objeto"],
+                :numeroAditivos => item["Número de Aditivos"],
+                :numeroProcessos => item["Número de Processos"],
+                :CPFContratada => item["CPF Contratada"],
+                :dataAssinatura => item["Data da Assinatura"],
+                :fundamentoGeral => item["Fundamento Geral"],
+                :dataInicioVigencia => item["Data de Inicio da Vigência"],
+                :dataTerminoVigencia => item["Data de Término da Vigência"],
+                :valorInicial => item["Valor Inicial"],
+                :valorInicial => item["Valor Inicial"],
+                :aditivosContrato => item["Aditivos Contrato"],
+                :eventosContrato => item["Eventos do Contrato"]
+            )
+    end
 end
