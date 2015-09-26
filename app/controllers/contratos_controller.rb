78d06f2ff2c77077b9a40cc4feed74dd13249db4
Nicacio Arruda <nicacionetobsb@hotmail.com>
2015-09-26 20:12:45 -0300

    functionality to show the false values ​​and modification of ContratosController class and show.html file

--- app/controllers/contratos_controller.rb
+++ app/controllers/contratos_controller.rb
@@ -3,14 +3,22 @@ class ContratosController < ApplicationController
   def index
        @contratos = Contrato.all
 
-
   end
 
   def show
+      @valorFalso1 = Float(Contrato.find(params["id"]).valorInicial)* 10
+      @valorFalso2 = Float(Contrato.find(params["id"]).valorInicial)* 15
+      @valorFalso3 = Float(Contrato.find(params["id"]).valorInicial)* 20
+
       puts "*" * 100, params["id"], "*" * 100
-       @contratos = Contrato.find(params["id"])
-       puts @contratos.id
-      
+      @contratos = Contrato.find(params["id"])
+      puts @contratos.id
+
+      #render json:@contratos
+      #render json:@valorFalso1
+      array = [@contratos.objeto,@valorFalso1,@valorFalso2,@valorFalso3]
+      render json:array
+
   end
 
   def edit
