69f36b87d13c588a1b1cf7a138f4c2c2d2b8bbed
ruanherculano <ruannawe95@gmail.com>
2015-09-27 18:37:26 -0300

    modification show numbers

--- app/controllers/contratos_controller.rb
+++ app/controllers/contratos_controller.rb
@@ -11,10 +11,9 @@ class ContratosController < ApplicationController
       @value_false_2 = Float(Contrato.find(params["id"]).valorInicial)*rand(30)/(rand(30))
       @value_false_3 = Float(Contrato.find(params["id"]).valorInicial)*rand(30)/(rand(30))
       @contratos = Contrato.find(params["id"])
-
-      array_value = [@value_true,@value_false_1,@value_false_2,@value_false_3]
+      array_value = [@value_true.round(0),@value_false_1.round(0),@value_false_2.round(0),@value_false_3.round(0)]
       array_objeto = [@contratos.objeto.capitalize]
-      array_question = [array_objeto, array_value.shuffle, @value_true]
+      array_question = [array_objeto, array_value.shuffle, @value_true.round(0)]
       array_question.flatten!
       render json:array_question
   end
