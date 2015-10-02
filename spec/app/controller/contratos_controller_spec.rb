0bef910b852c1908d55e1811289ea590ee2b0ad1
Sabryna <sabryna.sousa1323@gmail.com>
2015-10-01 23:00:03 -0300

    Implementeds Contrato controller tests

---
+++ spec/app/controller/contratos_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+RSpec.describe ContratosController, type: :controller do
+  describe "GET #Index" do
+    it "should show all contracts" do
+      contracts = Contrato.all
+      get :index
+      expect(contracts).to match(Contrato.all)
+    end
+  end
+
+    describe "GET #Show" do
+      it "assigns @contract" do
+          contract = Contrato.new
+          contract.identificadorContrato = "1"
+          contract.uasg = "1"
+          contract.modalidadeLicitacao = "1"
+          contract.codigoContrato = "1"
+          contract.licitacaoAssociada = "1"
+          contract.objeto = "1"
+          contract.numeroAditivos = 1
+          contract.numeroProcessos = 1
+          contract.CPFContratada = "1"
+          contract.dataAssinatura = "1"
+          contract.fundamentoGeral = "1"
+          contract.dataInicioVigencia = "1"
+          contract.dataTerminoVigencia = "1"
+          contract.valorInicial = "1"
+          contract.aditivosContratos = "1"
+          contract.eventosContratos = "1"
+
+          contract.save
+
+          get :show, id: contract.id
+          expect(assigns(:contratos)).to eq(contract)
+
+        end
+    end
+
+
+end
