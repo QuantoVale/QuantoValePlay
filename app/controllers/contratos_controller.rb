a179500b3e3053a26248a555eb0f7d7a487aa019
WildBillHiccup <gabrielv14@gmail.com>
2015-10-02 03:23:18 -0300

    Question JSON functional

--- app/controllers/contratos_controller.rb
+++ app/controllers/contratos_controller.rb
@@ -2,7 +2,7 @@ class ContratosController < ApplicationController
 
   def index
        @contratos = Contrato.all
-
+       render json:@contratos
   end
 
   def show
@@ -11,11 +11,11 @@ class ContratosController < ApplicationController
       @value_false_2 = Float(Contrato.find(params["id"]).valorInicial)*rand(30)/(rand(30))
       @value_false_3 = Float(Contrato.find(params["id"]).valorInicial)*rand(30)/(rand(30))
       @contratos = Contrato.find(params["id"])
-      array_value = [@value_true.round(0),@value_false_1.round(0),@value_false_2.round(0),@value_false_3.round(0)]
       array_objeto = [@contratos.objeto.capitalize]
-      array_question = [array_objeto, array_value.shuffle, @value_true.round(0)]
-      array_question.flatten!
-      render json:array_question
+
+      @array_question = {"name"=>array_objeto, "val1"=>@value_true.round(0), "val2"=>@value_false_1.round(0), "val3"=>@value_false_2.round(0), "val4"=>@value_false_3.round(0),"true" =>@value_true.round(0)}
+      #@array_question.flatten!
+      render json: @array_question
   end
 
   def edit
