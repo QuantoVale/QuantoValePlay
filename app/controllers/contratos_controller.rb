674a467004668bb8557d9e717f9366aa978a7d2d
ruanherculano <ruannawe95@gmail.com>
2015-10-14 23:37:17 -0300

    refactoring the code

--- app/controllers/contratos_controller.rb
+++ app/controllers/contratos_controller.rb
@@ -4,19 +4,21 @@ class ContratosController < ApplicationController
        @contratos = Contrato.all
        render json:@contratos
   end
-
+  
   def show
       @value_true = Float(Contrato.find(params["id"]).valorInicial)
-      @value_false_1 = Float(Contrato.find(params["id"]).valorInicial)*rand(30)/(rand(30))
-      @value_false_2 = Float(Contrato.find(params["id"]).valorInicial)*rand(30)/(rand(30))
-      @value_false_3 = Float(Contrato.find(params["id"]).valorInicial)*rand(30)/(rand(30))
+      @value_false_1 = Float(Contrato.find(params["id"]).valorInicial)*(1+rand(30))/(1+(rand(30)))
+      @value_false_2 = Float(Contrato.find(params["id"]).valorInicial)*(1+rand(30))/(1+(rand(30)))
+      @value_false_3 = Float(Contrato.find(params["id"]).valorInicial)*(1+rand(30))/(1+(rand(30)))
       @contratos = Contrato.find(params["id"])
-      array_objeto = [@contratos.objeto.capitalize]
-
-      @array_question = {"name"=>array_objeto, "val1"=>@value_true.round(0), "val2"=>@value_false_1.round(0), "val3"=>@value_false_2.round(0), "val4"=>@value_false_3.round(0),"true" =>@value_true.round(0)}
-      #@array_question.flatten!
-      render json: @array_question
-  end
+      @array_value = [@value_true,@value_false_1,@value_false_2,@value_false_3]
+      @array_value = [@value_true.round(0),@value_false_1.round(0),@value_false_2.round(0),@value_false_3.round(0)]
+      @array_objeto = [@contratos.objeto.capitalize]
+      @array_question = [@array_objeto, @array_value.shuffle, @value_true]
+      @array_question = [@array_objeto, @array_value.shuffle, @value_true.round(0)]
+      @array_question.flatten!
+      render json:@array_question
+    end
 
   def edit
 
