eeb09e456209949d99db1b5cf76cfc81ebdcadb5
ruanherculano <ruannawe95@gmail.com>
2015-10-18 23:25:47 -0200

    refactoring the code

--- app/controllers/contratos_controller.rb
+++ app/controllers/contratos_controller.rb
@@ -5,31 +5,27 @@ class ContratosController < ApplicationController
        render json:@contratos
   end
 
-  def values_aleatory ()
-    return @value_true*(1+rand(30))/(1+(rand(30)))
+  def values_aleatory value_true
+    value_true*(1+rand(30))/(1+(rand(30)))
   end
 
-  def applying_flatten()
-    @array_value = [@values_random, @value_true.round(0)]
-    return @array_value.flatten!
+  def merge values_random, value_true
+    array_value = [values_random, value_true.round(0)]
+  end
+
+  def applying_flatten array_value
+    array_value.flatten!
   end
 
   def show
-      @values_random = Array.new(3)
-      @value_true = Float(Contrato.find(params["id"]).valorInicial)
+      values_random = Array.new(3)
+      value_true = Float(Contrato.find(params["id"]).valorInicial).round(0)
       for i in 0..2
-          @values_random[i] = values_aleatory().round(0)
+          values_random[i] = values_aleatory(value_true).round(0)
       end
-      @array_question = [Contrato.find(params["id"]).objeto.capitalize, applying_flatten().shuffle, @value_true.round(0)]
-      @array_question.flatten!
-      render json:@array_question
-    end
-
-  def edit
-
-  end
-
-  def create
+      #.flatten junta .shuffle mistura
+      array_question = {"ask"=>Contrato.find(params["id"]).objeto.capitalize, "val"=>applying_flatten(merge(values_random, value_true)), "trueval"=>value_true}
+      render json:array_question
   end
 
 end
