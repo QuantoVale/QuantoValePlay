9245d2ef0bc3dd67f101cfe0537eddd077a05336
ruanherculano <ruannawe95@gmail.com>
2015-10-22 01:46:49 -0200

    implementing tests and adding a method in the controller to validate the random values ​​generated

--- app/controllers/contratos_controller.rb
+++ app/controllers/contratos_controller.rb
@@ -1,31 +1,41 @@
 class ContratosController < ApplicationController
-
   def index
        @contratos = Contrato.all
        render json:@contratos
   end
 
   def values_aleatory value_true
-    value_true*(1+rand(30))/(1+(rand(30)))
+    return value_true*(1+rand(30))/(1+(rand(30)))
   end
 
   def merge values_random, value_true
-    array_value = [values_random, value_true.round(0)]
+    return array_value = [values_random, value_true.round(0)]
+  end
+
+  def validate_values_aleatory value_validate, value_true
+    while value_validate == value_true
+      value_validate = values_aleatory(value_true)
+    end
+    return value_validate
   end
 
   def applying_flatten array_value
-    array_value.flatten!
+    return array_value.flatten!
+  end
+
+  def applying_shuffle array_question
+    return array_question.shuffle
   end
 
   def show
       values_random = Array.new(3)
       value_true = Float(Contrato.find(params["id"]).valorInicial).round(0)
       for i in 0..2
-          values_random[i] = values_aleatory(value_true).round(0)
+          values_random[i] = validate_values_aleatory(values_aleatory(value_true).round(0),value_true)
       end
       #.flatten junta .shuffle mistura
-      array_question = {"ask"=>Contrato.find(params["id"]).objeto.capitalize, "val"=>applying_flatten(merge(values_random, value_true)), "trueval"=>value_true}
+      array_question = {"ask"=>Contrato.find(params["id"]).objeto.capitalize,
+        "val"=>applying_shuffle(applying_flatten(merge(values_random, value_true))), "trueval"=>value_true}
       render json:array_question
   end
-
 end
