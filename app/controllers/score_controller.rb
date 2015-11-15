67d842a5ad3cb9d33d356e35a67e2150a237debd
Sabryna <sabryna.sousa1323@gmail.com>
2015-11-15 12:28:10 -0200

    Starting implementation score backend

---
+++ app/controllers/score_controller.rb
@@ -0,0 +1,29 @@
+class Score < ActionController::Base
+
+ resposta = false;
+ acertou = 0;
+ score = 0;
+
+  def calculation_score
+
+    if ( resposta  == true ){
+				acertou++;
+
+		if ( acertou < 3 )
+			score += 50;
+	  elsif ( acertou < 6 )
+			score += 100;
+	  elsif ( acertou < 9)
+			score += 200;
+		else
+			score += 400;
+	}
+
+	else{
+		acertou = 0;
+		score += 0;
+	}
+
+  end
+
+end
