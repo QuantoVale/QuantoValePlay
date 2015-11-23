c7021365921c8b58c0a4e3d4983af49c047a8ffa
matheus051022 <matheus.bionicle@gmail.com>
2015-11-23 13:54:53 -0200

    Time another time

--- frontend/www/js/controller.js
+++ frontend/www/js/controller.js
@@ -65,152 +65,148 @@ angular.module('starter.controllers', ['callRails', 'Score','ngResource'])
             });
         })
 
-    .controller('Answer', function($scope, ScoreEntry, ValuesService, $ionicPopup, $state, $ionicModal, $ionicSideMenuDelegate, $timeout, Interation) {
-        $scope.counter = 15;
-        $scope.jumpa = function(){
-        ValuesService.buttonPress().then(function(response) {
-             console.log(ValuesService.getPreviousId);
-             console.log(response.data);
-             $scope.values = response.data;
-             $score.counter = 15;
-         })
-     }
+        .controller('Answer', function($scope, ScoreEntry, ValuesService, $ionicPopup, $state, $ionicModal, $ionicSideMenuDelegate, $timeout) {
+            $scope.counter = 15;
+            $scope.jumpa = function(){
+            ValuesService.buttonPress().then(function(response) {
+                 console.log(ValuesService.getPreviousId);
+                 console.log(response.data);
+                 $scope.values = response.data;
+                 $score.counter = 15;
+             })
+         }
 
-        $scope.onTimeout = function(){
-          $scope.counter--;
-          if ($scope.counter > 0) {
-              mytimeout = $timeout($scope.onTimeout,1000);
-          }
-          else {
-            var confirmPopup = $ionicPopup.confirm({
-              title: 'Seu tempo Acabou!',
-              template: 'Deseja uma proxima questão ?'
+            $scope.onTimeout = function(){
+              $scope.counter--;
+              if ($scope.counter > 0) {
+                  mytimeout = $timeout($scope.onTimeout,1000);
+              }
+              else {
+                var confirmPopup = $ionicPopup.confirm({
+                  title: 'Seu tempo Acabou!',
+                  template: 'Deseja uma proxima questão ?'
+                });
+                confirmPopup.then(function(res){
+                  if(res){
+                    $scope.counter = 15;
+                    $scope.jumpa();
+                    $scope.onTimeout();
+                }
+                  else
+                  console.log('Cancelar encerramento');
+                })
+              }
+            }
+            $scope.reset= function(){
+              $scope.counter = 15;
+              $scope.onTimeout();
+            }
+            $scope.compare = function(x, y, id) {
+                var size = document.getElementsByTagName('span').length;
+                if (x === y) {
+                    document.getElementsByTagName('span')[id].style.backgroundColor = "#33cd5f";
+                    document.getElementsByTagName('span')[id].style.boxShadow = "0 8px 0 #28a54c";
+                    var answer = ScoreEntry.getTrue();
+                    if ( answer <= 3){
+                      var score = ScoreEntry.getScore();
+                    }
+                    else if ( answer > 3){
+                      score = ScoreEntry.getBonus();
+                    }
+
+                    document.getElementsByTagName('result')[0].innerHTML = score;
+                    $scope.certa = function(){
+                        return true;
+                    }
+                } else {
+                    document.getElementsByTagName('span')[id].style.boxShadow = "0 8px 0 #e42012";
+                    document.getElementsByTagName('span')[id].style.backgroundColor = "#ef473a";
+
+                    var answer = ScoreEntry.getAnswer();
+                    if (answer <= 3){
+                      var score = ScoreEntry.resetScore();
+                    }
+                    else if ( answer > 3){
+                      answer = ScoreEntry.getFalse();
+                      score =  ScoreEntry.getScore();
+                    }
+
+                    document.getElementsByTagName('result')[0].innerHTML = score;
+                    $scope.certa = function(){
+                        return false;
+                    }
+                }
+                var size = document.getElementsByTagName('li').length;
+                for(var i=0;i<size;i++){
+                    document.getElementsByTagName('li')[i].style.pointerEvents = "none";
+                }
+            }
+
+            $scope.clean = function() {
+                var size = document.getElementsByTagName('span').length;
+                for(var i=0;i<size;i++){
+                    document.getElementsByTagName('span')[i].style.backgroundColor = "#1E90FF";
+                    document.getElementsByTagName('span')[i].style.boxShadow = "0 8px 0 #4169E1";
+                }
+                var size = document.getElementsByTagName('li').length;
+                for(var i=0;i<size;i++){
+                    document.getElementsByTagName('li')[i].style.pointerEvents = "auto";
+                }
+                document.getElementsByTagName('result')[0].innerHTML = ' ';
+            }
+            $ionicModal.fromTemplateUrl('templates/answered.html', function($ionicModal) {
+                $scope.modal = $ionicModal;
+                console.log($scope.modal);
+            }, {
+                scope: $scope,
+                animation: 'slide-in-up',
+                backdropClickToClose: false,
+                hardwareBackButtonClose: false,
+                focusFirstInput: true
             });
-            confirmPopup.then(function(res){
-              if(res){
-                $scope.counter = 15;
-                $scope.jumpa();
-                $scope.onTimeout();
+            $scope.openModal = function() {
+                $scope.modal.show();
+            };
+            $scope.closeModal = function() {
+                $scope.modal.hide();
+            };
+            $scope.encerrar = function(){
+              var confirmPopup = $ionicPopup.confirm({
+                title: 'Fim da partida',
+                template: 'Deseja realmente encerrar a partida?'
+              });
+
+              confirmPopup.then(function(res){
+
+                if(res){
+                  $state.go('start.start');
+                  console.log('Encerrar');
+                }
+                else
+                console.log('Cancelar encerramento');
+              })
             }
-              else
-              console.log('Cancelar encerramento');
-            })
-          }
-        }
-        $scope.reset= function(){
-          $scope.counter = 15;
-          $scope.onTimeout();
-        }
-        $scope.compare = function(x, y, id) {
-            var size = document.getElementsByTagName('span').length;
-            if (x === y) {
-                document.getElementsByTagName('span')[id].style.backgroundColor = "#33cd5f";
-                document.getElementsByTagName('span')[id].style.boxShadow = "0 8px 0 #28a54c";
-                var answer = ScoreEntry.getTrue();
-                if ( answer <= 3){
-                  var score = ScoreEntry.getScore();
-                }
-                else if ( answer > 3){
-                  score = ScoreEntry.getBonus();
-                }
-                var player = {
-                    id: 2,
-                    score: 10
-                }
-                Interation.update(player);
-                document.getElementsByTagName('result')[0].innerHTML = score;
-                $scope.certa = function(){
-                    return true;
-                }
-            } else {
-                document.getElementsByTagName('span')[id].style.boxShadow = "0 8px 0 #e42012";
-                document.getElementsByTagName('span')[id].style.backgroundColor = "#ef473a";
 
-                var answer = ScoreEntry.getAnswer();
-                if (answer <= 3){
-                  var score = ScoreEntry.resetScore();
-                }
-                else if ( answer > 3){
-                  answer = ScoreEntry.getFalse();
-                  score =  ScoreEntry.getScore();
-                }
-
-                document.getElementsByTagName('result')[0].innerHTML = score;
-                $scope.certa = function(){
-                    return false;
-                }
+            $scope.pular = function(){
+                document.getElementsByTagName('result')[0].innerHTML = ' ';
             }
-            var size = document.getElementsByTagName('li').length;
-            for(var i=0;i<size;i++){
-                document.getElementsByTagName('li')[i].style.pointerEvents = "none";
-            }
-        }
 
-        $scope.clean = function() {
-            var size = document.getElementsByTagName('span').length;
-            for(var i=0;i<size;i++){
-                document.getElementsByTagName('span')[i].style.backgroundColor = "#1E90FF";
-                document.getElementsByTagName('span')[i].style.boxShadow = "0 8px 0 #4169E1";
-            }
-            var size = document.getElementsByTagName('li').length;
-            for(var i=0;i<size;i++){
-                document.getElementsByTagName('li')[i].style.pointerEvents = "auto";
-            }
-            document.getElementsByTagName('result')[0].innerHTML = ' ';
-        }
-        $ionicModal.fromTemplateUrl('templates/answered.html', function($ionicModal) {
-            $scope.modal = $ionicModal;
-            console.log($scope.modal);
-        }, {
-            scope: $scope,
-            animation: 'slide-in-up',
-            backdropClickToClose: false,
-            hardwareBackButtonClose: false,
-            focusFirstInput: true
-        });
-        $scope.openModal = function() {
-            $scope.modal.show();
-        };
-        $scope.closeModal = function() {
-            $scope.modal.hide();
-        };
-        $scope.encerrar = function(){
-          var confirmPopup = $ionicPopup.confirm({
-            title: 'Fim da partida',
-            template: 'Deseja realmente encerrar a partida?'
-          });
-
-          confirmPopup.then(function(res){
-
-            if(res){
-              $state.go('start.start');
-              console.log('Encerrar');
-            }
-            else
-            console.log('Cancelar encerramento');
-          })
-        }
-
-        $scope.pular = function(){
-            document.getElementsByTagName('result')[0].innerHTML = ' ';
-        }
-
-        ValuesService.buttonPress().then(function(response) {
-            console.log(ValuesService.getPreviousId);
-            console.log(response.data);
-            $scope.values = response.data;
-        })
-
-        $scope.jump = function(){
-           ValuesService.buttonPress().then(function(response) {
+            ValuesService.buttonPress().then(function(response) {
                 console.log(ValuesService.getPreviousId);
                 console.log(response.data);
                 $scope.values = response.data;
-                $scope.counter = 15;
-                $scope.timeout();
             })
-        }
-    })
+
+            $scope.jump = function(){
+               ValuesService.buttonPress().then(function(response) {
+                    console.log(ValuesService.getPreviousId);
+                    console.log(response.data);
+                    $scope.values = response.data;
+                    $scope.counter = 15;
+                    $scope.timeout();
+                })
+            }
+        })
 
     .controller('HomeCtrl', function($scope, $ionicPopup, $auth, OpenFB, $ionicSideMenuDelegate, Players) {
 
