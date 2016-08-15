9b31aa8d0fbbd73f075cf6aec6ec3f6d078fde20
Ruan <ruannawe95@gmail.com>
2016-08-15 02:06:02 -0300

    teste

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   def method_name
     puts ("teste")
   end
-  
+
   def find_player( id = 0 )
-                          
     assert { !id.nil? }
     id = id.to_i
     assert { id >= 0 }
     puts "teste"
     @player= Player.find(id);
-                          
   end
 
 end
